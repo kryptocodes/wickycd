4e308bb51a6652e7721567c3979a426a25fe41d7
kryptocodes <srivatsantb@gmail.com>
2023-12-09 01:45:48 +0530

    feat: added layout
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ layout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface indexProps {
+
+}
+
+const Layout: React.FC<indexProps> = ({}) => {
+        return (
+            <>
+            
+            </>
+        );
+}
+
+export default Layout
