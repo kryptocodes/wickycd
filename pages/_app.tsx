28fead4ecc928550440672ea93e3bb37abf9c6fe
kryptocodes <srivatsantb@gmail.com>
2023-12-10 00:16:27 +0530

    added huddle
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,16 @@ import type { AppProps } from "next/app";
 import WagmiProvider from "@/lib/wagmiProvider";
 
 
+import { HuddleClient, HuddleProvider } from "@huddle01/react";
+
+const huddleClient = new HuddleClient({
+  projectId: "zMQHa6hH5hGrxfwYZp7z8I-1lWScI7UA",
+  options: {
+    activeSpeakers: {
+      size: 8,
+    },
+  },
+});
 
 
 export const BE_URL = "http://localhost:5000/";
@@ -10,7 +20,9 @@ export const BE_URL = "http://localhost:5000/";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider>
+       <HuddleProvider client={huddleClient}>
       <Component {...pageProps} />
+      </HuddleProvider>
     </WagmiProvider>
   );
 }
